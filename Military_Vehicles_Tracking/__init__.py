3653de9b85ee1e8f4cd473e2062076aca8a20fe4
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-11-25 19:06:53 +0200

    fix path

--- Military_Vehicles_Tracking/__init__.py
+++ Military_Vehicles_Tracking/__init__.py
@@ -1,3 +1,1 @@
-from Military_Vehicles_Tracking.video import Video
-from Military_Vehicles_Tracking.tracker import MultiObjectTracker
-from Military_Vehicles_Tracking.plot_utils import plot_bounding_boxes
+
