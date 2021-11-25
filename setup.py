a8b5d56f70fe99f3a799fd0e9ecfec44ec1430d2
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-11-25 18:36:52 +0200

    update project setup

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
     name='Military_Vehicles_Tracking',
     version='1.0.0',
-    packages=['Military_Vehicles_Tracking'],
+    packages=['Military_Vehicles_Tracking', 'Military_Vehicles_Tracking.deep_sort_pytorch',
+              'Military_Vehicles_Tracking.deep_sort_pytorch.utils',
+              'Military_Vehicles_Tracking.deep_sort_pytorch.deep_sort',
+              'Military_Vehicles_Tracking.deep_sort_pytorch.deep_sort.deep',
+              'Military_Vehicles_Tracking.deep_sort_pytorch.deep_sort.sort'],
     url='https://github.com/Lin-Sinorodin/Military_Vehicles_Tracking',
     license='MIT',
     author='lin',
