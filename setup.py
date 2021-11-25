052da8cf24aee1ed6ae58214a32c8bb6d5bad0e9
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-11-25 18:25:07 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 setup(
     name='Military_Vehicles_Tracking',
     version='1.0.0',
-    packages=['src', 'src.deep_sort_pytorch', 'src.deep_sort_pytorch.utils', 'src.deep_sort_pytorch.deep_sort',
-              'src.deep_sort_pytorch.deep_sort.deep', 'src.deep_sort_pytorch.deep_sort.sort'],
+    packages=['src'],
     url='https://github.com/Lin-Sinorodin/Military_Vehicles_Tracking',
-    license='',
+    license='MIT',
     author='lin',
     author_email='linsino123@gmail.com',
     description='MOT for military vehicles'
