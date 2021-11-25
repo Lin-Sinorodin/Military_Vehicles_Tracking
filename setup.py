3589533216d4d6e6357ab0c702dd26225276adc4
‪Lin Sinorodin‬‏ <64460898+Lin-Sinorodin@users.noreply.github.com>
2021-11-25 18:31:43 +0200

    update project name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Military_Vehicles_Tracking',
     version='1.0.0',
-    packages=['src'],
+    packages=['Military_Vehicles_Tracking'],
     url='https://github.com/Lin-Sinorodin/Military_Vehicles_Tracking',
     license='MIT',
     author='lin',
